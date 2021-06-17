3716f12a78d8be8e57908d4281c72b29e6220dc6
dlamour <lamoure6@msu.edu>
2021-06-16 23:21:42 -0400

    js file update

--- soundboard.js
+++ soundboard.js
@@ -3,18 +3,19 @@ function playBoing(){
     boing.src = "boing.wav";
     boing.play();
 }
-document.getElementById("boing").onclick = playBoing;
 
 function playFloop(){
     var floop = new Audio();
     floop.src = "floop.wav";
     floop.play();
 }
-document.getElementById("floop").onclick = playFloop;
 
 function playBloop(){
     var bloop = new Audio();
     bloop.src = "bloop.wav";
     bloop.play();
 }
+
+document.getElementById("boing").onclick = playBoing;
+document.getElementById("floop").onclick = playFloop;
 document.getElementById("bloop").onclick = playBloop;
